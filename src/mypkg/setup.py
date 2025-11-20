9079528565730f7f51c75f7b780ed480fc1b601d
kz_abi <mikazuki503@gmail.com>
2025-11-20 12:31:21 +0900

    Initial commit

---
+++ src/mypkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kz503',
+    maintainer_email='mikazuki503@gmail.com',
+    description='robosys2025',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
+        ],
+    },
+)
