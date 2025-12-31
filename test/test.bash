d484ed51a5491cbbb5898c57a936f1e3f2f48415
kz_abi <mikazuki503@gmail.com>
2025-12-31 22:53:28 +0900

    Fix test script for CI environment

--- test/test.bash
+++ test/test.bash
@@ -7,18 +7,27 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source /opt/ros/humble/setup.bash
+source $dir/ros2_ws/install/setup.bash 
 
+#1. バックグラウンドでノードを起動
 ros2 launch mypkg mypkg.launch.py > /dev/null 2>&1 &
+PID=$! # プロセスIDを控える
 
+# 2. 起動待ち
 sleep 5
 
+# 3. トピックの値を15秒間監視してファイルに保存
 timeout 15 ros2 topic echo /current_val > /tmp/mypkg_log.txt
 
+# テスト終了後にノードをキルする
+kill $PID
+
+# 4. ログの中に "49.0" が含まれているかチェック
 if grep -q "49.0" /tmp/mypkg_log.txt; then
     echo "Test Passed: Target 50.0 reached!"
     exit 0
 else
     echo "Test Failed: Target 50.0 not reached."
+    cat /tmp/mypkg_log.txt # ログの中身を表示（デバッグ用）
     exit 1
 fi
