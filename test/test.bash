20e009749cba4a239a0f8500fb54f1d151f2420d
kz_abi <mikazuki503@gmail.com>
2025-12-31 22:32:31 +0900

    Add GitHub Actions and test script

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 kz-abi
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -6,7 +8,17 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+ros2 launch mypkg mypkg.launch.py > /dev/null 2>&1 &
+
+sleep 5
+
+timeout 15 ros2 topic echo /current_val > /tmp/mypkg_log.txt
+
+if grep -q "49.0" /tmp/mypkg_log.txt; then
+    echo "Test Passed: Target 50.0 reached!"
+    exit 0
+else
+    echo "Test Failed: Target 50.0 not reached."
+    exit 1
+fi
